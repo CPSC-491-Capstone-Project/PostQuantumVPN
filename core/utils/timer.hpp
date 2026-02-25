e748bde7d4b5bea9fb2480ac565c77ce6c1b8acf
DrChrisHax <cmanlove1234@outlook.com>
2026-02-25 13:49:22 -0800

    Added timer class

---
+++ core/utils/timer.hpp
@@ -0,0 +1,48 @@
+#ifndef _PQVPN_CORE_UTILS_TIMER_HPP_
+#define _PQVPN_CORE_UTILS_TIMER_HPP_
+
+#include <chrono>
+#include <string>
+#include <string_view>
+#include <iostream>
+
+namespace core::utils {
+
+    class Timer {
+    public:
+        using Clock = std::chrono::steady_clock;
+        using TimePoint = Clock::time_point;
+        using Duration = std::chrono::nanoseconds;
+
+        void Start() {
+            start_ = Clock::now();
+        }
+
+        void Stop() {
+            end_= Clock::now();
+        }
+
+        [[nodiscard]] Duration Elapsed() const {
+            return std::chrono::duration_cast<Duration>(end_- start_);
+        }
+
+        [[nodiscard]] std::string ElapsedStr() const {
+            auto ns = Elapsed().count();
+            if (ns < 1'000LL) 
+                return std::to_string(ns) + " ns";
+            if (ns < 1'000'000LL)
+                return std::to_string(ns / 1'000.0) + " us";
+            if (ns < 1'000'000'000LL)
+                return std::to_string(ns / 1'000'000.0) + " ms";
+            return std::to_string(ns / 1'000'000'000.0) + " s";
+        }
+
+    private:
+        TimePoint start_{};
+        TimePoint end_{};
+
+    }
+
+} // namespace core:utils
+
+#endif // _PQVPN_CORE_UTILS_TIMER_HPP_
