b5f27ad2c9fcead8ce0eefefe348255a4b741570
DrChrisHax <cmanlove1234@outlook.com>
2026-02-25 16:38:52 -0800

    Added Byte Encode/Decode functions

--- core/utils/bit_utils.hpp
+++ core/utils/bit_utils.hpp
@@ -2,12 +2,15 @@
 #define _PQVPN_CORE_UTILS_BIT_UTILS_HPP_
 
 #include <cstdint>
-#include <vector>
+#include <span>
 
 namespace core::utils {
 
-    std::vector<uint8_t> BitsToBytes(const std::vector<uint8_t>& bits);
-    std::vector<uint8_t> BytesToBits(const std::vector<uint8_t>& bytes);
+    // Converts a bit array (of length that is a multiple of eight) into an array of bytes.
+    void BitsToBytes(std::span<const uint8_t> bits, std::span<uint8_t> bytes);
+
+    // Performs the inverse of BitsToBytes, converting a byte array into a bit array
+    void BytesToBits(std::span<const uint8_t> bytes, std::span<uint8_t> bits);
 
 } // namespace core::utils
 
