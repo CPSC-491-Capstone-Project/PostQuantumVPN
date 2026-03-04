9c4930633301d0b8ba321e8da62e54d363e24ddb
HunterTran10 <hunter.q.tran@gmail.com>
2026-03-04 15:42:34 -0800

    DG-149 Implement logger levels

---
+++ core/utils/LogEvent.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "LogLevel.h"
+#include <ctime>
+
+// A single log event that holds the level, timestamp and message
+struct LogEvent {
+    LogLevel   level;
+    time_t     timestamp;
+    std::string message;
+
+    LogEvent(LogLevel lvl, std::string msg) {
+        level     = lvl;
+        timestamp = time(0);
+        message   = msg;
+    }
+
+    // Higher level takes priority, ties broken by earliest timestamp
+    bool operator<(const LogEvent& other) const {
+        if (level != other.level)
+            return level < other.level;
+        return timestamp > other.timestamp;
+    }
+};
