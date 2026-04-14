1d6d5d9b6666f9bffa1676b705ab6702784fbac3
Quan Truong <quantruongh571@gmail.com>
2026-04-13 21:56:48 -0700

    Update udp_socket for macos

--- core/os/macos/udp_socket.cpp
+++ core/os/macos/udp_socket.cpp
@@ -8,6 +8,8 @@
 #include <unistd.h>
 #include <cstring>
 #include <utility>
+#include <optional>
+#include <string>
 
 using core::utils::Logger;
 
@@ -45,7 +47,8 @@ namespace core::network {
         return true;
     }
 
-    bool UDPSocket::Bind(const std::string& ip, std::uint16_t port) {
+  
+    bool UDPSocket::Bind(const IPv4 ip, Port port) {
         if (!IsOpen()) {
             Logger::Error("UDPSocket: Bind called on closed socket");
             return false;
@@ -55,13 +58,13 @@ namespace core::network {
         addr.sin_family = AF_INET;
         addr.sin_port = htons(port);
 
-        if (::inet_pton(AF_INET, ip.c_str(), &addr.sin_addr) != 1) {
-            Logger::Error("UDPSocket: Invalid bind address: " + ip);
+        if (::inet_pton(AF_INET, ip.ToString().c_str(), &addr.sin_addr) != 1) {
+            Logger::Error("UDPSocket: Invalid bind address: " + ip.ToString());
             return false;
         }
 
         if (::bind(handle_, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) < 0) {
-            Logger::Error("UDPSocket: Failed to bind to " + ip + ":" + std::to_string(port));
+            Logger::Error("UDPSocket: Failed to bind to " + ip.ToString() + ":" + std::to_string(port));
             return false;
         }
 
@@ -107,8 +110,9 @@ namespace core::network {
         addr.sin_family = AF_INET;
         addr.sin_port = htons(destination.port);
 
-        if (::inet_pton(AF_INET, destination.ip.c_str(), &addr.sin_addr) != 1) {
-            Logger::Error("UDPSocket: Invalid destination address: " + destination.ip);
+        
+        if (::inet_pton(AF_INET, destination.ip.ToString().c_str(), &addr.sin_addr) != 1) {
+            Logger::Error("UDPSocket: Invalid destination address: " + destination.ip.ToString());
             return -1;
         }
 
@@ -154,10 +158,11 @@ namespace core::network {
         char ip_buffer[INET_ADDRSTRLEN]{};
         ::inet_ntop(AF_INET, &addr.sin_addr, ip_buffer, sizeof(ip_buffer));
 
+      
         return ReceiveResult{
             .bytes_read = static_cast<std::size_t>(received),
             .sender = Endpoint{
-                .ip = std::string(ip_buffer),
+                .ip = IPv4(std::string(ip_buffer)),
                 .port = ntohs(addr.sin_port)
             }
         };
@@ -180,8 +185,9 @@ namespace core::network {
         char ip_buf[INET_ADDRSTRLEN]{};
         ::inet_ntop(AF_INET, &addr.sin_addr, ip_buf, sizeof(ip_buf));
 
+       
         return Endpoint{
-            .ip = std::string(ip_buf),
+            .ip = IPv4(std::string(ip_buf)),
             .port = ntohs(addr.sin_port)
         };
     }
